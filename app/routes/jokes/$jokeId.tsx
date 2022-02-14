c7d640d34fd2baeef1453151f3ee523146d3ca5d
Felipe Freitag Vargas <ffvargas@gmail.com>
2022-02-14 11:53:38 -0300

    optimistic UI

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -12,8 +12,9 @@ import {
 import type { Joke } from '@prisma/client'
 import { db } from '~/utils/db.server'
 import { getUserId, requireUserId } from '~/utils/session.server'
+import { JokeDisplay } from '~/components/joke'
 
-type LoaderData = { joke: Joke; isOwner: Boolean }
+type LoaderData = { joke: Joke; isOwner: boolean }
 
 export const meta: MetaFunction = ({
   data,
@@ -69,23 +70,8 @@ export const action: ActionFunction = async ({ request, params }) => {
 
 export default function JokeRoute() {
   const data = useLoaderData<LoaderData>()
-  const { name, content } = data.joke
 
-  return (
-    <div>
-      <p>Here's your hilarious joke:</p>
-      <p>{content}</p>
-      <Link to=".">{name} Permalink</Link>
-      {data.isOwner && (
-        <Form method="post">
-          <input type="hidden" name="_method" value="delete" />
-          <button type="submit" className="button">
-            Delete
-          </button>
-        </Form>
-      )}
-    </div>
-  )
+  return <JokeDisplay joke={data.joke} isOwner={data.isOwner} />
 }
 
 export function CatchBoundary() {
