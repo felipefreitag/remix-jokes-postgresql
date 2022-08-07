03ddaad0162d2450a79245777017e4a8eac7ce77
Felipe Freitag Vargas <ffvargas@gmail.com>
2022-08-07 13:48:42 -0300

    Handle loader must throw

--- app/routes/jokes/__tests__/jokeid.test.ts
+++ app/routes/jokes/__tests__/jokeid.test.ts
@@ -4,11 +4,15 @@ describe('loader', () => {
   it('fails without an id', async () => {
     const request = new Request('http://foo.ber')
 
+    let result
     try {
       await loader({ request, context: {}, params: {} })
     } catch (error) {
-      expect((error as Response).status).toEqual(400)
+      result = error
     }
+
+    expect((result as Response).status).toEqual(400)
+
     // Todo: assert loader has thrown
   })
 })
