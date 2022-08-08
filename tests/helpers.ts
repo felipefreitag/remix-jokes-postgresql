ec94d412e4a2b97ba3936115dbeb08e23732b381
Felipe Freitag Vargas <ffvargas@gmail.com>
2022-08-08 14:49:40 -0300

    Clear db after each test

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -1,23 +1,13 @@
-import { PrismaClient } from '@prisma/client'
 import { db } from '~/utils/db.server'
 
 const testTransaction = (fnWithinTransaction: () => Promise<unknown>) => {
-  const oldDb = db
-  const connection = new PrismaClient()
   const test = async () => {
-    await connection.$transaction(async (prisma) => {
-      // @ts-ignore
-      global.__db = prisma
-
-      try {
-        await prisma.$executeRaw`SET timezone TO 'UTC'`
-        await fnWithinTransaction()
-      } finally {
-        await prisma.$executeRaw`ROLLBACK`
-        global.__db = oldDb
-      }
-    })
-    connection.$disconnect()
+    try {
+      await db.$executeRaw`SET timezone TO 'UTC'`
+      await fnWithinTransaction()
+    } finally {
+      await db.$executeRaw`TRUNCATE TABLE users CASCADE`
+    }
   }
 
   return test
